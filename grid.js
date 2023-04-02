68f84a6bda0e7771f2ef18762d3c83f93bd7dac5
Muhammad Huseyn <m.badaev@staq.dev>
2023-04-02 23:41:32 +0300

    #1 - module build for game

---
+++ grid.js
@@ -0,0 +1,13 @@
+const GRID_SIZE = 4;
+const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
+
+export class Grid {
+    constructor(gridElement) {
+        this.cells = [];
+        for (let i = 0; i < CELLS_COUNT; i++) {
+            this.cells.push(
+                 new Cell(gridElement, x, y)
+            )
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard); 
